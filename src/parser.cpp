33eba1bc55d52a92452248d202afe1ad6e5d765a
Colin Edwards <colin@recursivepenguin.com>
2013-02-22 14:04:59 -0600

    Add set text methods to span and block elements

--- src/parser.cpp
+++ src/parser.cpp
@@ -174,7 +174,8 @@ static void rndr_header(struct buf *ob, struct buf *text, int level, void *opaqu
 	//if there is no hash
 	checkDataMove(ob, opaque);
 
-	Bypass::BlockElement* element = new Bypass::BlockElement(text->data);
+	Bypass::BlockElement* element = new Bypass::BlockElement();
+	element->setText(text->data);
 	stackTempElement(element, opaque);
 }
 
