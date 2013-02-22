50b9de77f8de6decac779ad3f45b9c64b184b637
Damian Carrillo <dcarrillo@ironclad.mobi>
2013-02-22 15:05:49 -0600

    Getting callbacks into C++ as quickly as possible

--- src/parser.h
+++ src/parser.h
@@ -19,10 +19,37 @@ namespace Bypass {
 	public:
 		Parser();
 		~Parser();
+
 		Document parse(const char *markdown);
 		Document parse(const std::string &markdown);
+
+		// Parsing Internals
+
 		void moveTempToDocument();
 		void stackTempElement(BlockElement* blockElement);
+
+		// Block Element Callbacks
+
+		void parsedBlockcode(struct buf *ob, struct buf *text);
+		void parsedBlockquote(struct buf *ob, struct buf *text);
+		void parsedHeader(struct buf *ob, struct buf *text, int level);
+		void parsedList(struct buf *ob, struct buf *text, int flags);
+		void parsedListItem(struct buf *ob, struct buf *text, int flags);
+		void parsedParagraph(struct buf *ob, struct buf *text);
+
+		// Span Element Callbacks
+
+		int parsedCodeSpan(struct buf *ob, struct buf *text);
+		int parsedDoubleEmphasis(struct buf *ob, struct buf *text, char c);
+		int parsedEmphasis(struct buf *ob, struct buf *text, char c);
+		int parsedTripleEmphasis(struct buf *ob, struct buf *text, char c);
+		int parsedLinebreak(struct buf *ob);
+		int parsedLink(struct buf *ob, struct buf *link, struct buf *title, struct buf *content);
+
+		// Low Level Callbacks
+
+		void parsedNormalText(struct buf *ob, struct buf *text);
+
 	private:
 		Document* document;
 		BlockElement* tempBlockElement;
