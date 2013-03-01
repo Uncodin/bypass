3a6aeae15bef18bbc3ca753768674ea1dc7b1855
Colin Edwards <colin@recursivepenguin.com>
2013-03-01 15:03:44 -0600

    Delete references that JNI holds. Show and example readme.

--- platform/android/tests/src/in/uncod/android/bypass/test/BenchmarkActivity.java
+++ platform/android/tests/src/in/uncod/android/bypass/test/BenchmarkActivity.java
@@ -1,8 +1,11 @@
 package in.uncod.android.bypass.test;
 
+import java.io.InputStream;
+
 import in.uncod.android.bypass.Bypass;
 import in.uncod.andrpod.bypass.bypass_test.R;
 import android.app.Activity;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
 import android.widget.TextView;
@@ -17,10 +20,24 @@ public class BenchmarkActivity extends Activity {
 		
 		TextView text = (TextView) findViewById(R.id.demoText);
 		Bypass bypass = new Bypass();
-		CharSequence string = bypass.markdownToSpannable("Header!!\n--------\nText goes here or something\n*One* **Two** ***Three***\n[Link](https://github.com/Uncodin/bypass)\n\n-\tOne\n-\tTwo\n\n");
+		String readme = loadFile();
+		CharSequence string = bypass.markdownToSpannable(readme);
 		text.setText(string);
 		//Allows link clicking
 		text.setMovementMethod(LinkMovementMethod.getInstance());
 	}
 
+	private String loadFile() {
+		try {
+	        Resources res = getResources();
+	        InputStream in_s = res.openRawResource(R.raw.readme);
+
+	        byte[] b = new byte[in_s.available()];
+	        in_s.read(b);
+	        return new String(b);
+	    } catch (Exception e) {
+	        // e.printStackTrace();
+	    }
+		return "";
+	}
 }
