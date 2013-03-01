16b2757380134515012316d7279d5fb237d8fcd7
Colin Edwards <colin@recursivepenguin.com>
2013-03-01 11:07:40 -0600

    Android tests. Ability to render headers

--- platform/android/src/in/uncod/android/bypass/Bypass.java
+++ platform/android/src/in/uncod/android/bypass/Bypass.java
@@ -1,20 +1,28 @@
 package in.uncod.android.bypass;
 
+import in.uncod.android.bypass.Element.Type;
+import android.graphics.Typeface;
 import android.text.Spannable;
 import android.text.SpannableStringBuilder;
 import android.text.TextUtils;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
 
 public class Bypass {
 	static {
-		System.loadLibrary("spandown");
+		System.loadLibrary("bypass");
 	}
 
+	private static final float[] HEADER_SIZES = {
+	        1.5f, 1.4f, 1.3f, 1.2f, 1.1f, 1f,
+	};
+	
 	public CharSequence markdownToSpannable(String markdown) {
 		Document document = processMarkdown(markdown);
 		
-		Spannable[] spans = new Spannable[document.getElementCount()];
+		CharSequence[] spans = new CharSequence[document.getElementCount()];
 		for (int i=0; i<document.getElementCount(); i++) {
-			spans[i] = elementToSpannable(document.getElement(i));
+			spans[i] = recurseElement(document.getElement(i));
 		}
 		
 		return TextUtils.concat(spans);
@@ -22,11 +30,28 @@ public class Bypass {
 	
 	private native Document processMarkdown(String markdown);
 	
-	private Spannable elementToSpannable(Element element) {
-		SpannableStringBuilder stringBuilder = new SpannableStringBuilder();
+	private CharSequence recurseElement(Element element) {
 		
-		//MAGIC!!
+		CharSequence[] spans = new CharSequence[element.size()];
+		for (int i=0; i<element.size(); i++) {
+			spans[i] = recurseElement(element.children[i]);
+		}
 		
-		return stringBuilder;
+		CharSequence concat = TextUtils.concat(spans);
+		
+		SpannableStringBuilder builder = new SpannableStringBuilder();
+		builder.append(element.getText());
+		builder.append(concat);
+		
+		if (element.getType() == Type.HEADER) {
+			String levelStr = element.getAttribute("level");
+			int level = Integer.parseInt(levelStr);
+			builder.setSpan(new RelativeSizeSpan(HEADER_SIZES[level]),
+					0, builder.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            builder.setSpan(new StyleSpan(Typeface.BOLD),
+            		0, builder.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+		}
+		
+		return builder;
 	}
 }
---
+++ platform/android/tests/Bypass-Test-FrameworkTest/src/in/uncod/andrpod/bypass/bypass_test/test/BasicTests.java
@@ -0,0 +1,37 @@
+package in.uncod.andrpod.bypass.bypass_test.test;
+
+import in.uncod.android.bypass.Bypass;
+import android.test.InstrumentationTestCase;
+import android.text.Spannable;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
+import android.util.Log;
+
+public class BasicTests extends InstrumentationTestCase {
+
+	Bypass bypass;
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+
+		bypass = new Bypass();
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testEmptyString() {
+		bypass.markdownToSpannable("");
+	}
+
+	public void testHeaderString() {
+		Spannable spannable = (Spannable) bypass.markdownToSpannable("Header!!\n--------");
+		StyleSpan[] spans = spannable.getSpans(0, spannable.length(), StyleSpan.class);
+		assertEquals(1, spans.length);
+		RelativeSizeSpan[] sizeSpan = spannable.getSpans(0, spannable.length(), RelativeSizeSpan.class);
+		assertEquals(1, sizeSpan.length);
+	}
+}
