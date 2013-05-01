d1901e206a3d3f07a738b400dcb38f652c1b0ff1
Audun Holm Ellertsen <audune@gmail.com>
2013-05-01 15:20:41 +0100

    Release fonts if necessary

--- platform/ios/Bypass/Bypass/BPDisplaySettings.m
+++ platform/ios/Bypass/Bypass/BPDisplaySettings.m
@@ -31,13 +31,13 @@
 {
   self = [super init];
   if (self) {
-    self.bulletIndentation     = 13.0f;
-    self.codeIndentation       = 10.0f;
-    self.quoteIndentation      = 23.0f;
-    self.lineSpacingSmall      =  1.2f;
+    self.bulletIndentation = 13.0f;
+    self.codeIndentation = 10.0f;
+    self.quoteIndentation = 23.0f;
+    self.lineSpacingSmall = 1.2f;
     self.paragraphSpacingLarge = 20.0f;
     self.paragraphSpacingSmall = 10.0f;
-    self.paragraphSpacingNone  =  0.0f;
+    self.paragraphSpacingNone = 0.0f;
   }
   return self;
 }
@@ -45,130 +45,135 @@
 
 - (void)dealloc
 {
-    if (_defaultFont != NULL)    CFRelease(_defaultFont);
-    if (_boldFont != NULL)       CFRelease(_boldFont);
-    if (_italicFont != NULL)     CFRelease(_italicFont);
-    if (_boldItalicFont != NULL) CFRelease(_boldItalicFont);
-    if (_monospaceFont != NULL)  CFRelease(_monospaceFont);
-    if (_quoteFont != NULL)      CFRelease(_quoteFont);
-    if (_h1Font != NULL)         CFRelease(_h1Font);
-    if (_h2Font != NULL)         CFRelease(_h2Font);
-    if (_h3Font != NULL)         CFRelease(_h3Font);
-    if (_h4Font != NULL)         CFRelease(_h4Font);
-    if (_h5Font != NULL)         CFRelease(_h5Font);
-    if (_h6Font != NULL)         CFRelease(_h6Font);
+  if (_defaultFont != NULL) {CFRelease(_defaultFont);}
+  if (_boldFont != NULL) {CFRelease(_boldFont);}
+  if (_italicFont != NULL) {CFRelease(_italicFont);}
+  if (_boldItalicFont != NULL) {CFRelease(_boldItalicFont);}
+  if (_monospaceFont != NULL) {CFRelease(_monospaceFont);}
+  if (_quoteFont != NULL) {CFRelease(_quoteFont);}
+  if (_h1Font != NULL) {CFRelease(_h1Font);}
+  if (_h2Font != NULL) {CFRelease(_h2Font);}
+  if (_h3Font != NULL) {CFRelease(_h3Font);}
+  if (_h4Font != NULL) {CFRelease(_h4Font);}
+  if (_h5Font != NULL) {CFRelease(_h5Font);}
+  if (_h6Font != NULL) {CFRelease(_h6Font);}
 }
 
 - (void)setDefaultFont:(CTFontRef)defaultFont
 {
+  if (_defaultFont) {CFRelease(_defaultFont);}
   _defaultFont = defaultFont;
 }
 
 - (void)setBoldFont:(CTFontRef)boldFont
 {
+  if (_boldFont) {CFRelease(_boldFont);}
   _boldFont = boldFont;
 }
 
 - (void)setItalicFont:(CTFontRef)italicFont
 {
+  if (_italicFont) {CFRelease(_italicFont);}
   _italicFont = italicFont;
 }
 
 - (void)setBoldItalicFont:(CTFontRef)boldItalicFont
 {
+  if (_boldItalicFont) {CFRelease(_boldItalicFont);}
   _boldItalicFont = boldItalicFont;
 }
 
 - (void)setMonospaceFont:(CTFontRef)monospaceFont
 {
+  if (_monospaceFont) {CFRelease(_monospaceFont);}
   _monospaceFont = monospaceFont;
 }
 
 - (void)setQuoteFont:(CTFontRef)quoteFont
 {
+  if (_quoteFont) {CFRelease(_quoteFont);}
   _quoteFont = quoteFont;
 }
 
 - (void)setH1Font:(CTFontRef)h1Font
 {
+  if (_h1Font) {CFRelease(_h1Font);}
   _h1Font = h1Font;
 }
 
 - (void)setH2Font:(CTFontRef)h2Font
 {
+  if (_h2Font) {CFRelease(_h2Font);}
   _h2Font = h2Font;
 }
 
 - (void)setH3Font:(CTFontRef)h3Font
 {
+  if (_h3Font) {CFRelease(_h3Font);}
   _h3Font = h3Font;
 }
 
 - (void)setH4Font:(CTFontRef)h4Font
 {
+  if (_h4Font) {CFRelease(_h4Font);}
   _h4Font = h4Font;
 }
 
 - (void)setH5Font:(CTFontRef)h5Font
 {
+  if (_h5Font) {CFRelease(_h5Font);}
   _h5Font = h5Font;
 }
 
 - (void)setH6Font:(CTFontRef)h6Font
 {
+  if (_h6Font) {CFRelease(_h6Font);}
   _h6Font = h6Font;
 }
 
 - (CTFontRef)defaultFont
 {
-    if (_defaultFont == NULL) {
-        CGFloat systemFontSize = [UIFont systemFontSize];
-        UIFont *systemFont = [UIFont systemFontOfSize:systemFontSize];
-        CFStringRef systemFontName = (__bridge CFStringRef) [systemFont fontName];
 
-        _defaultFont = CTFontCreateWithName(systemFontName, systemFontSize, NULL);
-    }
+  if (_defaultFont == NULL) {
+    CGFloat systemFontSize = [UIFont systemFontSize];
+    UIFont *systemFont = [UIFont systemFontOfSize:systemFontSize];
+    CFStringRef systemFontName = (__bridge CFStringRef) [systemFont fontName];
+
+    _defaultFont = CTFontCreateWithName(systemFontName, systemFontSize, NULL);
+  }
   assert(_defaultFont != NULL);
   return _defaultFont;
 }
 
 - (CTFontRef)boldFont
 {
-    if (_boldFont == NULL) {
-        _boldFont = CTFontCreateCopyWithSymbolicTraits([self defaultFont],
-                                                       0.f,
-                                                       NULL,
-                                                       kCTFontBoldTrait,
-                                                       kCTFontBoldTrait);
-    }
+  if (_boldFont == NULL) {
+    _boldFont = CTFontCreateCopyWithSymbolicTraits([self defaultFont], 0.f, NULL, kCTFontBoldTrait, kCTFontBoldTrait);
+  }
   assert(_boldFont != NULL);
   return _boldFont;
 }
 
 - (CTFontRef)italicFont
 {
+  if (_italicFont == NULL) {
+    _italicFont = CTFontCreateCopyWithSymbolicTraits([self defaultFont], 0.f, NULL, kCTFontItalicTrait, kCTFontItalicTrait);
     if (_italicFont == NULL) {
-        _italicFont = CTFontCreateCopyWithSymbolicTraits([self defaultFont],
-                                                         0.f,
-                                                         NULL,
-                                                         kCTFontItalicTrait,
-                                                         kCTFontItalicTrait);
-      if (_italicFont == NULL) {
-        CGFloat systemFontSize = [UIFont systemFontSize];
-        UIFont *systemFont = [UIFont italicSystemFontOfSize:systemFontSize];
-        CFStringRef systemFontName = (__bridge CFStringRef) [systemFont fontName];
+      CGFloat systemFontSize = [UIFont systemFontSize];
+      UIFont *systemFont = [UIFont italicSystemFontOfSize:systemFontSize];
+      CFStringRef systemFontName = (__bridge CFStringRef) [systemFont fontName];
 
-        _italicFont = CTFontCreateWithName(systemFontName, systemFontSize, NULL);
-      }
-      if (_italicFont == NULL) {
-        CGAffineTransform matrix = {1, 0, 0.11/* skew angle */, 1, 0, 0};
-        _italicFont = CTFontCreateCopyWithAttributes([self defaultFont], 0.0, &matrix, NULL);
-        CFStringRef fontName = CTFontCopyFullName([self defaultFont]);
-        NSLog(@"Warning: can't find an italic font variant for font %@. Fallback to matrix/skew used.",
-              (__bridge NSString *) fontName);
-        CFRelease(fontName);
-      }
+      _italicFont = CTFontCreateWithName(systemFontName, systemFontSize, NULL);
     }
+    if (_italicFont == NULL) {
+      CGAffineTransform matrix = {1, 0, 0.11/* skew angle */, 1, 0, 0};
+      _italicFont = CTFontCreateCopyWithAttributes([self defaultFont], 0.0, &matrix, NULL);
+      CFStringRef fontName = CTFontCopyFullName([self defaultFont]);
+      NSLog(@"Warning: can't find an italic font variant for font %@. Fallback to matrix/skew used.",
+            (__bridge NSString *) fontName);
+      CFRelease(fontName);
+    }
+  }
 
   assert(_italicFont != NULL);
   return _italicFont;
@@ -176,96 +181,84 @@
 
 - (CTFontRef)boldItalicFont
 {
-    if (_boldItalicFont == NULL) {
-        CTFontSymbolicTraits traits = kCTFontBoldTrait | kCTFontItalicTrait;
-        CTFontSymbolicTraits mask = kCTFontBoldTrait | kCTFontItalicTrait;
-        _boldItalicFont = CTFontCreateCopyWithSymbolicTraits([self defaultFont],
-                                                             0.f,
-                                                             NULL,
-                                                             traits,
-                                                             mask);
-    }
+  if (_boldItalicFont == NULL) {
+    CTFontSymbolicTraits traits = kCTFontBoldTrait | kCTFontItalicTrait;
+    CTFontSymbolicTraits mask = kCTFontBoldTrait | kCTFontItalicTrait;
+    _boldItalicFont = CTFontCreateCopyWithSymbolicTraits([self defaultFont], 0.f, NULL, traits, mask);
+  }
   assert(_boldItalicFont != NULL);
   return _boldItalicFont;
 }
 
 - (CTFontRef)monospaceFont
 {
-    if (_monospaceFont == NULL) {
-        _monospaceFont = CTFontCreateWithName(CFSTR("Courier"),
-                                              CTFontGetSize([self defaultFont]) - 2, NULL);
-    }
+  if (_monospaceFont == NULL) {
+    _monospaceFont = CTFontCreateWithName(CFSTR("Courier"), CTFontGetSize([self defaultFont]) - 2, NULL);
+  }
   assert(_monospaceFont != NULL);
   return _monospaceFont;
 }
 
 - (CTFontRef)quoteFont
 {
-    if (_quoteFont == NULL) {
-        _quoteFont = CTFontCreateWithName(CFSTR("Marion-Italic"),
-                                          CTFontGetSize([self defaultFont]) + 2, NULL);
-    }
+  if (_quoteFont == NULL) {
+    _quoteFont = CTFontCreateWithName(CFSTR("Marion-Italic"), CTFontGetSize([self defaultFont]) + 2, NULL);
+  }
 
-    return _quoteFont;
+  return _quoteFont;
 }
 
 - (CTFontRef)h1Font
 {
-    if (_h1Font == NULL) {
-        _h1Font = CTFontCreateWithName(CFSTR("HelveticaNeue-CondensedBold"),
-                                       CTFontGetSize([self defaultFont]) * 2, NULL);
-    }
+  if (_h1Font == NULL) {
+    _h1Font = CTFontCreateWithName(CFSTR("HelveticaNeue-CondensedBold"), CTFontGetSize([self defaultFont]) * 2, NULL);
+  }
 
-    return _h1Font;
+  return _h1Font;
 }
 
 - (CTFontRef)h2Font
 {
-    if (_h2Font == NULL) {
-        _h2Font = CTFontCreateWithName(CFSTR("HelveticaNeue-CondensedBold"),
-                                       CTFontGetSize([self defaultFont]) * 1.8, NULL);
-    }
+  if (_h2Font == NULL) {
+    _h2Font = CTFontCreateWithName(CFSTR("HelveticaNeue-CondensedBold"), CTFontGetSize([self defaultFont]) * 1.8, NULL);
+  }
 
-    return _h2Font;
+  return _h2Font;
 }
 
 - (CTFontRef)h3Font
 {
-    if (_h3Font == NULL) {
-        _h3Font = CTFontCreateWithName(CFSTR("HelveticaNeue-CondensedBold"),
-                                       CTFontGetSize([self defaultFont]) * 1.6, NULL);
-    }
+  if (_h3Font == NULL) {
+    _h3Font = CTFontCreateWithName(CFSTR("HelveticaNeue-CondensedBold"), CTFontGetSize([self defaultFont]) * 1.6, NULL);
+  }
 
-    return _h3Font;
+  return _h3Font;
 }
 
 - (CTFontRef)h4Font
 {
-    if (_h4Font == NULL) {
-        _h4Font = CTFontCreateWithName(CFSTR("HelveticaNeue-CondensedBold"),
-                                       CTFontGetSize([self defaultFont]) * 1.4, NULL);
-    }
+  if (_h4Font == NULL) {
+    _h4Font = CTFontCreateWithName(CFSTR("HelveticaNeue-CondensedBold"), CTFontGetSize([self defaultFont]) * 1.4, NULL);
+  }
 
-    return _h4Font;
+  return _h4Font;
 }
 
 - (CTFontRef)h5Font
 {
-    if (_h5Font == NULL) {
-        _h5Font = CTFontCreateWithName(CFSTR("HelveticaNeue-CondensedBold"),
-                                       CTFontGetSize([self defaultFont]) * 1.2, NULL);
-    }
+  if (_h5Font == NULL) {
+    _h5Font = CTFontCreateWithName(CFSTR("HelveticaNeue-CondensedBold"), CTFontGetSize([self defaultFont]) * 1.2, NULL);
+  }
 
-    return _h5Font;
+  return _h5Font;
 }
 
 - (CTFontRef)h6Font
 {
-    if (_h6Font == NULL) {
-        _h6Font = CTFontCreateWithName(CFSTR("HelveticaNeue-CondensedBold"),
-                                       CTFontGetSize([self defaultFont]) * 1, NULL);
-    }
+  if (_h6Font == NULL) {
+    _h6Font = CTFontCreateWithName(CFSTR("HelveticaNeue-CondensedBold"), CTFontGetSize([self defaultFont]) * 1, NULL);
+  }
 
-    return _h6Font;
+  return _h6Font;
 }
 @end
