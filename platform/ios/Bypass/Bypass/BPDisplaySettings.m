0a7ba5414a0981fe387c42439fd0adce21dba3ec
peepo3663 <peepo157@gmail.com>
2015-01-15 15:25:36 +0700

    Edit render Bold Italic font to used boldItalicFontRef
    I found problem in rendering bold and italic font which it didn't use bold italic font ref.

--- platform/ios/Bypass/Bypass/BPDisplaySettings.m
+++ platform/ios/Bypass/Bypass/BPDisplaySettings.m
@@ -58,8 +58,9 @@
     CTFontRef boldItalicFontRef = CTFontCreateCopyWithSymbolicTraits(defaultFontRef, 0.f, NULL, traits, mask);
     assert(boldItalicFontRef != NULL);
 
-    _boldItalicFont = [self UIFontFromCTFont:defaultFontRef];
+    _boldItalicFont = [self UIFontFromCTFont:boldItalicFontRef];
     CFRelease(defaultFontRef);
+    CFRelease(boldItalicFontRef);
   }
 
   return _boldItalicFont;
